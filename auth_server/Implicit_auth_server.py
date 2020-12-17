7584da61046b98a1699ae13e309ea1dfab563fb5
Ganesh Kanna <mail2sgk@gmail.com>
2020-12-17 21:20:03 +0530

    Update Implicit_auth_server.py

--- auth_server/Implicit_auth_server.py
+++ auth_server/Implicit_auth_server.py
@@ -2,7 +2,7 @@ import json
 import ssl
 import urllib.parse as urlparse
 
-from auth import (authenticate_user_credentials, generate_access_token,  
+from auth import (authenticate_user_credentials, generate_access_token,
                   verify_client_info, JWT_LIFE_SPAN)
 from flask import Flask, redirect, render_template, request
 from urllib.parse import urlencode
@@ -20,76 +20,68 @@ app = Flask(__name__)
 
 
 
-@app.route('validate')
+@app.route('/validate')
 def validate():
-  print(request.args.get('access_token')
+    print(request.args.get('access_token')
 
-
+# Describe the access request of the client and ask user for approval
 @app.route('/auth')
 def auth():
-  # Describe the access request of the client and ask user for approval
-  client_id = request.args.get('client_id')
-  redirect_url = request.args.get('redirect_uri')
-  state = request.args.get('state')
-  print(client_id)
-  print(redirect_url)
+    client_id = request.args.get('client_id')
+    redirect_url = request.args.get('redirect_uri')
+    state = request.args.get('state')
+    print(client_id)
+    print(redirect_url)
+    if None in [ client_id, redirect_url ]:
+        return json.dumps({
+        "error": "invalid_request"
+        }), 400
+    if not verify_client_info(client_id, redirect_url):
+        return json.dumps({
+        "error": "invalid_client"
+        })
 
-  if None in [ client_id, redirect_url ]:
-    return json.dumps({
-      "error": "invalid_request"
-    }), 400
-
-  if not verify_client_info(client_id, redirect_url):
-    return json.dumps({
-      "error": "invalid_client"
-    })
-
-  return render_template('Implicit_grant_access.html',
-                         client_id = client_id,
-                         redirect_url = redirect_url,state=state)
+    return render_template('Implicit_grant_access.html',client_id = client_id,redirect_url = redirect_url,state=state)
 
 def process_redirect_url(redirect_url,new_entries):
+
   # Prepare the redirect URL
-  url_parts = list(urlparse.urlparse(redirect_url))
-  print(list)
-  queries = dict(urlparse.parse_qsl(url_parts[4]))
-  queries.update(new_entries)
-  url_parts[4] = urlencode(queries)
-  url = urlparse.urlunparse(url_parts)
-  print(url)
-  url = url.replace("?","#")
+    url_parts = list(urlparse.urlparse(redirect_url))
+    print(list)
+    queries = dict(urlparse.parse_qsl(url_parts[4]))
+    queries.update(new_entries)
+    url_parts[4] = urlencode(queries)
+    url = urlparse.urlunparse(url_parts)
+    print(url)
+    url = url.replace("?","#")
   #url = url.replace('&access_token','#access_token')
-  return url
+    return url
 
 @app.route('/signin', methods = ['POST'])
 def signin():
   # Issues authorization code
-  username = request.form.get('username')
-  password = request.form.get('password')
-  client_id = request.form.get('client_id')
-  redirect_url = request.form.get('redirect_url')
-  state = request.form.get('state')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    client_id = request.form.get('client_id')
+    redirect_url = request.form.get('redirect_url')
+    state = request.form.get('state')
 
-  if None in [username, password, client_id, redirect_url]:
-    return json.dumps({
-      "error": "invalid_request"
-    }), 400
+    if None in [username, password, client_id, redirect_url]:
+        return json.dumps({"error": "invalid_request"}), 400
 
-  if not verify_client_info(client_id, redirect_url):
-    return json.dumps({
-      "error": "invalid_client"
-    })  
+    if not verify_client_info(client_id, redirect_url):
+        return json.dumps({"error": "invalid_client"})
 
-  if not authenticate_user_credentials(username, password):
-    return json.dumps({
-      'error': 'access_denied'
-    }), 401
+    if not authenticate_user_credentials(username, password):
+        return json.dumps({
+        'error': 'access_denied'
+        }), 401
 
-  access_token = generate_access_token()
+    access_token = generate_access_token()
 
-  print(process_redirect_url(redirect_url, {"1":"2"}))
+    print(process_redirect_url(redirect_url, {"1":"2"}))
 
-  return redirect(process_redirect_url(redirect_url,{
+    return redirect(process_redirect_url(redirect_url,{
     'access_token': access_token,
     'token_type': 'bearer',
     'expires_in': JWT_LIFE_SPAN,
@@ -98,7 +90,8 @@ def signin():
 
 
 if __name__ == '__main__':
+    app.run(host='0.0.0.0',port = 443, debug = True,ssl_context = context)
+    
   #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
   #context.load_cert_chain('domain.crt', 'domain.key')
   #app.run(port = 5000, debug = True, ssl_context = context)
-  app.run(host='0.0.0.0',port = 443, debug = True,ssl_context = context)
